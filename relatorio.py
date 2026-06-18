67d5e06b7918ea9bae8337b0507e33374354b4ae
Pedro recla <pedroori27@gmail.com>
2026-06-18 08:48:25 -0300

    Adiciona uma correção

--- relatorio.py
+++ relatorio.py
@@ -2,7 +2,7 @@ def gerar_relatorio(lista):
     total = len(lista)
     concluidas = sum(1 for t in lista if t["concluida"])
     pendentes = total - concluidas
-    print("\n===== RELATÓRIO =====")
+    print("\n===== RELATÓRIOS =====")
     print(f"Total      : {total}")
     print(f"Concluídas : {concluidas}")
     print(f"Pendentes  : {pendentes}")
